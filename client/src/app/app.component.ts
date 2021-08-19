f71d6e3422cb070caa2d6ea1a5c45093c963cc67
mehedi-bjit <hasan.mehedi@bjitgroup.com>
2021-08-19 12:06:28 +0600

    implement the navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(`${environment.apiUrl}/products?pageSize=50`).subscribe(
+      (res: IPagination) => {
+        this.products = res.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
