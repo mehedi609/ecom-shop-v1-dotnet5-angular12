6fa8323a15b67fe5c9c50436bcae6867836389bf
mehedi-bjit <hasan.mehedi@bjitgroup.com>
2021-08-27 21:39:21 +0600

    implemented basic routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { ShopModule } from './shop/shop.module';
     HttpClientModule,
     CoreModule,
     ShopModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
