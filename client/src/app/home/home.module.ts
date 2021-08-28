82357b2535bbb67885a6bd1e0b4f2fc592be6a3c
mehedi-bjit <hasan.mehedi@bjitgroup.com>
2021-08-29 02:04:50 +0600

    implemented carousel in the home page

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CarouselModule.forRoot(), RouterModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
