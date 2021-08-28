80c5cc3f00cd90b8cdb57411ed76623bebe9618d
mehedi-bjit <hasan.mehedi@bjitgroup.com>
2021-08-28 19:58:29 +0600

    implemented breadcrumb functionality

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,22 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'server-error', component: ServerErrorComponent },
-  { path: 'not-found', component: NotFoundComponent },
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not Found' },
+  },
   {
     path: 'shop',
     loadChildren: () =>
       import('./shop/shop.module').then((mod) => mod.ShopModule),
+    data: { breadcrumb: 'Shop' },
   },
   {
     path: 'test-error',
@@ -19,8 +28,9 @@ const routes: Routes = [
       import('./test-error/test-error.module').then(
         (mod) => mod.TestErrorModule
       ),
+    data: { breadcrumb: 'Test Error' },
   },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+  { path: '**', pathMatch: 'full', redirectTo: 'not-found' },
 ];
 
 @NgModule({
