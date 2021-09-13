f68354bb0c54cf8c79a8cd51216f1faaf7b1f4be
Mehedi <mehedi609@gmail.com>
2021-09-13 17:16:46 +0600

    created OrdersController.cs

---
+++ API/Dtos/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace API.Dtos
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string RetrieveEmailFromPrincipal(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
